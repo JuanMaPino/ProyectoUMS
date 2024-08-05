9f8eb39368f693c440821f987f520947ff39aae2
Alg-888 <alesonicx23@gmail.com>
2024-08-05 06:56:45 -0500

    backend actividadesxproyectos

--- frontend/src/api/ApiActividad.jsx
+++ frontend/src/api/ApiActividad.jsx
@@ -6,6 +6,7 @@ export const createActividadRequest = (actividad) => axios.post(`${API}/activida
 export const updateActividadRequest = (id, actividad) => axios.put(`${API}/actividades/${id}`, actividad);
 export const getActividadByIdRequest = (id) => axios.get(`${API}/actividades/${id}`);
 export const getAllActividadesRequest = () => axios.get(`${API}/actividades/`);
+export const getActividadesByProyectoRequest = (proyectoId) => axios.get(`${API}/actividades/proyecto/${proyectoId}`); // Nueva función
 export const disableActividadRequest = (id) => axios.patch(`${API}/actividades/${id}/estado`);
 export const searchActividadByNameRequest = (nombre) => axios.get(`${API}/actividades/buscar/${nombre}`);
 export const deleteActividadRequest = (id) => axios.delete(`${API}/actividades/${id}`);
