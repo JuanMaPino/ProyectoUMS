2181697c43409249bede3aa8452920117044dd28
Alg-888 <alesonicx23@gmail.com>
2024-09-19 02:09:51 -0500

    Actualizacion 1.1.0

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,11 @@ app.use(cors({
   origin: 'http://localhost:3000',
   credentials: true
 }));
+
+app.use(cors({
+  origin: 'http://localhost:8081',
+  credentials: true
+}));
 app.use(morgan('dev'));
 app.use(express.json());
 
